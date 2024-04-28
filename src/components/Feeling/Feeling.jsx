3575731582cf68d1d27bf1c8dfdf2b198851985e
Kenneth Grina <grina.kenneth@gmail.com>
2024-04-28 11:21:42 -0500

    feeling page

---
+++ src/components/Feeling/Feeling.jsx
@@ -0,0 +1,12 @@
+import FeedbackRating from "../FeedbackRating/FeedbackRating";
+
+function Feeling() {
+    return(
+        <>
+        <h3>How are you feeling today?</h3>
+        <FeedbackRating />
+        </>
+    )
+}
+
+export default Feeling;
